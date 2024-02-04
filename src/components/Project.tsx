198bf95fb72a8c1c15f704daba933bf66e1af19f
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-05 00:00:00 +0100

    edit import

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -75,8 +75,6 @@ export default function Project({
         <Image
           src={imageUrl}
           alt='Project I worked on'
-          width={500}
-          height={500}
           quality={95}
           className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
         transition 
